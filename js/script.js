6e35226bd1c70923ac9c1619afb970d482f511ad
Tuan Ha <hatuan@email.cz>
2023-11-11 02:12:12 +0100

    add display character info function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Code for slideshow
+ */
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -7,16 +10,40 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.querySelectorAll(".mySlides");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
+  let i;
+  let slides = document.querySelectorAll(".mySlides");
+  if (n > slides.length) {
+      slideIndex = 1
   }
+  if (n < 1) {
+      slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+/**
+ * Code for character display
+ */
+
+//based on img id
+const characters = ["isagi", "chigiri", "bachira", "kunigami"];
+const characterInfo = ["info-isagi", "info-chigiri", "info-bachira", "info-kunigami"];
+let infoToDisplay = "info-";
+
+characters.forEach(function(id) {
+  let img = document.querySelector(`#${id}`);
+  img.addEventListener("click", function() {
+    const defaultInfo = document.querySelector(".default-info");
+    defaultInfo.style.display = "none";
+
+    characterInfo.forEach(function(infoId) {
+      document.querySelector(`#${infoId}`).style.display = "none";
+    });
+
+    document.querySelector(`#${infoToDisplay+id}`).style.display = "block";
+    img.style.opacity = "1";
+  });
+});
